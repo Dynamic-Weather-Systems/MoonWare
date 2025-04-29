cf54a7a31b67a8dedbd0efcb9a23026e6031d5e8
sanjitiguess <63969681+sanjitiguess@users.noreply.github.com>
2025-04-28 22:56:30 -0400

    boulder
    -added boulder
    -updated character movement
    -updated better sprites

--- moonware/microgame/dr_jones_running/character_body_2d.gd
+++ moonware/microgame/dr_jones_running/character_body_2d.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+@export var SPEED = 300.0
 
 
 func _physics_process(delta: float) -> void:	
 	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction < 0:
+		$animated.flip_h = true
+	elif direction > 0:
+		$animated.flip_h = false
+	
 	if direction:
+		$animated.play("default")
 		velocity.x = direction * SPEED
 	else:
+		$animated.pause()
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
