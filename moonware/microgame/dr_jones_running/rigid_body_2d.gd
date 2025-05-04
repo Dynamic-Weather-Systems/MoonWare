621f30af7e3bde65e194aa1f780a055b79d16982
sanjitiguess <63969681+sanjitiguess@users.noreply.github.com>
2025-05-03 20:57:57 -0400

    almost finished indiana jones
     - fully working signals
     - inputs changed to left mouse
     - still needs ending animations (TODO)

--- moonware/microgame/dr_jones_running/rigid_body_2d.gd
+++ moonware/microgame/dr_jones_running/rigid_body_2d.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
-@export var rotation_speed = 200
-@export var running_speed = 10
+@export var rotation_speed = 200 # speed of the boulder spinning
+@export var running_speed = 10 # speed of the boulder moving right
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Sprite2D.rotation_degrees +=rotation_speed
-	position.x += running_speed
+	$Sprite2D.rotation_degrees +=rotation_speed # rotates the boulder
+	position.x += running_speed # moves the boulder
 	
