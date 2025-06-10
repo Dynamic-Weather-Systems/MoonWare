131a88c03971a2803015258200387e082147a59a
sanjitiguess <63969681+sanjitiguess@users.noreply.github.com>
2025-06-10 11:23:37 -0400

    removed paralax nodes
     - removed paralax nodes
     - added math code to manually create paralax
     - added code to make it run smoother

--- moonware/microgame/dr_jones_running/character_body_2d.gd
+++ moonware/microgame/dr_jones_running/character_body_2d.gd
@@ -3,15 +3,19 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0 # speed of the player running
 var direction = 0 # direction of the player
+var old_pos
 
 func _ready() -> void:
 	$animated.play("default")
+	old_pos = global_position
 
 
 func _physics_process(delta: float) -> void:
 	direction = move_toward(direction, 0, 0.05) # moves direction toward 0 if no clicking
 	
-	if direction: # if moving
+	if $animated.animation == 'explode':
+		velocity.x = move_toward(velocity.x, 0, SPEED) # stop moving
+	elif direction: # if moving
 		$animated.play("default") # play running animation
 		velocity.x = direction * SPEED # add to the velocity of the player
 	elif $animated.animation == 'default': # if not moving and not explodings
@@ -19,6 +23,18 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED) # stop moving
 	
 	move_and_slide() # update movements
+	%cave.global_position += (old_pos - global_position)/2
+	%cave2.global_position += (old_pos - global_position)/2
+	%stalagmite.global_position += old_pos - global_position
+	%stalagmite2.global_position += old_pos - global_position
+	%stalagmite3.global_position += old_pos - global_position
+	%stalagmite4.global_position += old_pos - global_position
+	%stalagmite5.global_position += old_pos - global_position
+	%stalagmite6.global_position += old_pos - global_position
+	$"..".global_position += old_pos - global_position
+	if %boulder:
+		%boulder.global_position += old_pos - global_position
+	old_pos = global_position
 
 
 func _input(event: InputEvent) -> void: # if input is called
