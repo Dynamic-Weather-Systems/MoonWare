b8e7e0e941f6bad2af539edd96eedfcd7eabff64
sanjitiguess <63969681+sanjitiguess@users.noreply.github.com>
2025-06-23 14:32:04 -0400

    Weather randomizer
     - added a way to randomize the weather on a day, and the temperature
     - added sprites to for weather icons
     - added randomizing the icons

--- moonware/microgame/match_weather/weather_main.gd
+++ moonware/microgame/match_weather/weather_main.gd
@@ -3,10 +3,22 @@ extends Microgame
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	randomize()
 	super() # calls the _ready function of microgame main
+	
+	var labels = [$Sunny, $Cloudy, $"Partly Cloudy", $Showers, $Stormy]
+	var sprites = [$sunny_sprite, $cloudy_sprite, $partly_cloudy_sprite, $showers_sprite, $stormy_sprite]
+	labels.shuffle()
+	sprites.shuffle()
+	for i in range(len(labels)):
+		labels[i].global_position.x = ((90 * (i+1)) - 50) + (i-2) * 15
+		sprites[i].global_position.x = (90 * (i+1))
+		var rand_temp = Vector2(randi_range(15,30), randi_range(15,30))
+		labels[i].text = str(labels[i].name) + '\nHi    Lo\n' + str(max(rand_temp.x,rand_temp.y)) + '   ' + str(min(rand_temp.x,rand_temp.y))
+	
 	countdown.start() # starts the timer
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+	#pass
