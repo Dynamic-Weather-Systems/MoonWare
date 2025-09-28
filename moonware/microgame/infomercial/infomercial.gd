45a1e0cf6d68707bd9d49e6ad219a03c4ff72bd7
sanjitiguess <63969681+sanjitiguess@users.noreply.github.com>
2025-09-28 19:48:04 -0400

    barebones infomercial completed
     - added win state and lose state
     - preliminary sounds
     - mouse movement checkers
     - end win/lose message
     - some assets to use

--- moonware/microgame/infomercial/infomercial.gd
+++ moonware/microgame/infomercial/infomercial.gd
@@ -1,11 +1,45 @@
 extends Microgame
 
+var has_won := false
+var has_lost := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	super()
+	%EndMessage.text = ''
+	countdown.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+	#pass
+
+
+func _on_mouse_area_mouse_exited() -> void:
+	_mouse_moved()
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("any_mouse_action"):
+		_mouse_moved()
+
+func _on_countdown_timeout():
+	%MouseArea.queue_free()
+	%background.stop()
+	
+	if !has_won and !has_lost:
+		has_won = true
+		%win.play()
+		%EndMessage.text = win_message
+
+
+func _mouse_moved():
+	if !has_won and !has_lost:
+		has_lost = true
+		%background.stop()
+		%lose.play()
+		%EndMessage.text = lose_message
+		%MouseArea.queue_free()
+
+
+func _on_background_finished() -> void:
+	%background.play()
